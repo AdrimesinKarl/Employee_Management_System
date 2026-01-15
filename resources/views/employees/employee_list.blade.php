4a3dcb0b10b30425c6e3b406fbc76dd99804f4dd
AdrimesinKarl <jcarlo1002@gmail.com>
2026-01-15 16:37:27 +0800

    fixed some errors

--- resources/views/employees/employee_list.blade.php
+++ resources/views/employees/employee_list.blade.php
@@ -4,21 +4,23 @@
         <title>Employee List</title>
     </head>
     <body>
+        <h1>Employee List</h1>
         <table border="1">
         <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <ht>Email</ht>
-            <ht>hourly_rate</ht>
+            <th>firstname</th>
+            <th>lastname</th>
+            <th>email</th>
+            <th>hourly_rate</th>
         </tr>
         @foreach($employees as $employee)
         <tr>
-            <td>{{ $employee->Firstname }}</td>
-            <td>{{ $employee->Lastname}}</td>
-            <td>{{ $employee->Email}}</td>
+            <td>{{ $employee->first_name }}</td>
+            <td>{{ $employee->last_name }}</td>
+            <td>{{ $employee->email }}</td>
             <td>{{ $employee->hourly_rate }}</td>
         </tr>
         @endforeach
+        @extends('layouts.app')
     </table>
     </body>
 </html>
